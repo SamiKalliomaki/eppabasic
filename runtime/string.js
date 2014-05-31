6bcaae3d61343d841b27f40ce73df3038f1d9482
Henrik Lievonen <henrik.lievonen@hotmail.com>
2014-05-31 16:05:08 +0300

    Adds missing runtime/string.js

---
+++ runtime/string.js
@@ -0,0 +1,28 @@
+﻿function EbString(heap) {
+    this.MEMU8 = new Uint8Array(heap);
+    this.MEMS32 = new Int32Array(heap);
+    this.MEMF32 = new Float32Array(heap);
+
+    // Make all functions to use right this
+    for (func in this.env) {
+        if (this.env.hasOwnProperty(func))
+            this.env[func] = this.env[func].bind(this);
+    }
+}
+
+EbString.prototype = {
+    env: {
+        print: function print(sp) {
+            var ptr = this.MEMS32[(sp - 4) >> 2];
+            var len = this.MEMS32[ptr >> 2];
+            var buf = [];
+            for (var i = 0; i < len; i++) {
+                buf.push(String.fromCharCode(this.MEMU8[ptr + i + 8]));
+            }
+            var str = buf.join('');
+            alert(str);
+        }
+    },
+
+    stdlib: {}
+};
