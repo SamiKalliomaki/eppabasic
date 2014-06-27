ff4518f0abb827fcad6d0d017b7c1ece3950c232
Sami Kalliomäki <sami.kalliomaki@kapsi.fi>
2014-06-27 07:30:00 +0300

    Fixes admin password change form

--- eppabasic_backend/users/admin.py
+++ eppabasic_backend/users/admin.py
@@ -1,9 +1,13 @@
 from django.db import transaction
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.utils.decorators import method_decorator
+from django.utils.html import escape
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.debug import sensitive_post_parameters
-from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.contrib import admin, messages
 from django.contrib.admin.options import IS_POPUP_VAR
 from django.contrib.auth.forms import AdminPasswordChangeForm
 from users.forms import CustomUserChangeForm, CustomUserCreationForm
