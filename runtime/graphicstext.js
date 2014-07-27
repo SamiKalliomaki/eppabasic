a31e9d48ee2d846eeb4c3c8ef7809ad89b8b24c3
Henrik Lievonen <henrik.lievonen@hotmail.com>
2014-07-27 22:39:58 +0300

    Moves string conversion to a new StringUtil class.

--- runtime/graphicstext.js
+++ runtime/graphicstext.js
@@ -1,10 +1,10 @@
-﻿function GraphicsText(canvasHolder, heap) {
+﻿/// <reference path="utils/string.js" />
+
+function GraphicsText(canvasHolder, heap) {
     this.console = document.createElement('div');
     canvasHolder.appendChild(this.console);
 
-    this.MEMU8 = new Uint8Array(heap);
-    this.MEMS32 = new Int32Array(heap);
-    this.MEMF32 = new Float32Array(heap);
+    this.strUtil = new StringUtils(heap);
 
     // Make all functions to use right this
     for (func in this.env) {
@@ -29,24 +29,7 @@ GraphicsText.prototype = {
         },
 
         printStr: function print(str) {
-            var len = this.MEMS32[str >> 2];
-            var i = 0;
-            var buf = [];
-            while (i < len) {
-                var charcode = this.MEMU8[str + 4 + i];
-                for (var j = 7; j >= 0; j--) {
-                    if (!(charcode & (1 << j)))
-                        break;
-                    charcode ^= 1 << j;
-                }
-                i++;
-                while ((this.MEMU8[str + 4 + i] & 0x80) && !(this.MEMU8[str + 4 + i] & 0x40) && i < len) {
-                    charcode = (charcode << 6) | (0x3f & this.MEMU8[str + 4 + i]);
-                    i++;
-                }
-                buf.push(String.fromCodePoint(charcode));
-            }
-            str = buf.join('');
+            str = this.strUtil.toString(str);
 
             alert(str);
         },
---
+++ runtime/utils/string.js
@@ -0,0 +1,28 @@
+﻿function StringUtils(heap) {
+    this.heap = heap;
+    this.MEMS32 = new Int32Array(heap);
+    this.MEMU8 = new Int8Array(heap);
+}
+
+StringUtils.prototype = {
+    toString: function toString(ptr) {
+        var len = this.MEMS32[ptr >> 2];
+        var i = 0;
+        var buf = [];
+        while (i < len) {
+            var charcode = this.MEMU8[ptr + 4 + i];
+            for (var j = 7; j >= 0; j--) {
+                if (!(charcode & (1 << j)))
+                    break;
+                charcode ^= 1 << j;
+            }
+            i++;
+            while ((this.MEMU8[ptr + 4 + i] & 0x80) && !(this.MEMU8[ptr + 4 + i] & 0x40) && i < len) {
+                charcode = (charcode << 6) | (0x3f & this.MEMU8[ptr + 4 + i]);
+                i++;
+            }
+            buf.push(String.fromCodePoint(charcode));
+        }
+        return buf.join('');
+    }
+};
