3fbb4bf7a9d5538fb1075c2642df87b98056b069
Sami Kalliomäki <sami.kalliomaki@kapsi.fi>
2014-07-27 17:44:08 +0300

    Default drawing colors

--- runtime/runtime.js
+++ runtime/runtime.js
@@ -37,6 +37,10 @@ Runtime.prototype = {
     loadLibraries: function loadLibraries() {
         this.g2d = new Graphics2D(this.canvasHolder, this.heap);
         this.g2d.setSize(640, 480);
+        this.g2d.ctx.strokeStyle = 'rgb(255, 255, 255)';
+        this.g2d.ctx.fillStyle = 'rgb(255, 255, 255)';
+        this.g2d.clearColor = 'rgb(0, 0, 0)';
+        this.g2d.env.clear();
 
         mixin(this.env, this.g2d.env);
 
