2e019bd00a219ad161ce3c189999502b3c67d619
Sami Kalliomäki <sami.kalliomaki@kapsi.fi>
2014-07-28 16:03:54 +0300

    Ace is now built on the server

--- ace/build/src/snippets/worker-eppabasic.js
+++
@@ -1,7 +0,0 @@
-define("ace/snippets/worker-eppabasic",["require","exports","module"], function(require, exports, module) {
-"use strict";
-
-exports.snippetText =undefined;
-exports.scope = "worker-eppabasic";
-
-});
