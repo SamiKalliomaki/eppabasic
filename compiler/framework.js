38c0e217c33065666133b701f09540036acc1b8d
Henrik Lievonen <henrik.lievonen@hotmail.com>
2014-08-01 02:10:08 +0300

    Fixes types in errors causing i18next to crash

--- compiler/framework.js
+++ compiler/framework.js
@@ -1,5 +1,11 @@
 function CompileError(line, msg,data) {
     this.line = line;
     this.msg = msg;
+    // Convert data types to string
+    for (var d in data) {
+        if (data.hasOwnProperty(d)) {
+            data[d] = data[d].toString();
+        }
+    }
     this.data = data;
 }
