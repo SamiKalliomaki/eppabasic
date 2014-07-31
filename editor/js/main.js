6774d3d31f9c09856e9d053982d32aa57ba61909
Henrik Lievonen <henrik.lievonen@hotmail.com>
2014-08-01 00:36:58 +0300

    In case of user exiting the page, the code is saved to sessionStorage and recovered when the page is reloaded

--- editor/js/main.js
+++ editor/js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     var notificationSystem = new NotificationSystem('#notification-box');
     var editor = new Editor('editor');
     var userControlsController = new UserControlsController('#user-controls');
@@ -12,38 +12,49 @@ $(function() {
 
     window.ebeditor = editor;
 
-    i18n.init({ fallbackLng: 'en-US' }, function(t) {
+    i18n.init({ fallbackLng: 'en-US' }, function (t) {
         $('#language-selection').val(i18n.options.lng);
 
-        $('#language-selection').change(function() {
-            i18n.setLng($('#language-selection').val(), function(t) {
+        $('#language-selection').change(function () {
+            i18n.setLng($('#language-selection').val(), function (t) {
                 $('body').i18n();
             });
         });
-        
+
         $('body').i18n();
     });
 
     $('#editor').resizable({
-        resize: function(e, ui) {
+        resize: function (e, ui) {
             $('#manual').css('margin-left', ui.size.width + 2);
         },
         handles: "e"
     });
 
-    $('.dialog-wrapper').click(function(e) {
-        if(e.target !== this)
+    $('.dialog-wrapper').click(function (e) {
+        if (e.target !== this)
             return;
 
         $(this).hide();
     });
 
-    // Tell user about page reload
-    window.onbeforeunload = function(e) {
-        var msg = i18n.t('confirm-leave');
+    // Restore save code
+    if (sessionStorage && sessionStorage.getItem('code')) {
+        editor.setCode(sessionStorage.getItem('code'));
+    }
+    // Save code to sessionStorage if possible
+    // Otherwise just show a message telling that the code will be lost
+    window.onbeforeunload = function (e) {
+        if (sessionStorage) {
+            // Save the code to the sessionStorage
+            sessionStorage.setItem('code', editor.getCode());
+        } else {
+            // Show the message
+            var msg = i18n.t('confirm-leave');
 
-        e.returnValue = msg;
-        return msg;
+            e.returnValue = msg;
+            return msg;
+        }
     };
 });
 
