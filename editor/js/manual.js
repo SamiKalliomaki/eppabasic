7ea32659aa95607733cb5d72574675fabd1a119a
Henrik Lievonen <henrik.lievonen@hotmail.com>
2014-07-31 23:19:59 +0300

    Manual has now a back button

--- editor/js/manual.js
+++ editor/js/manual.js
@@ -1,28 +1,42 @@
-function Manual(manual, lang) {
-    this.manual = $(manual);
+function Manual(manualContainer, lang) {
+    manualContainer = $(manualContainer);
+    this.manual = $(manualContainer.find('#manual'));
+    this.back = $(manualContainer.find('.back'));
     this.lang = lang;
 
+    this.history = [];
+
     var me = this;
-    this.manual.on('click', 'a', function(e) {
+
+    this.manual.on('click', 'a', function (e) {
         e.preventDefault();
 
         var href = $(this).attr('href');
         var manualKey = 'manual:';
 
-        if(href.substring(0, manualKey.length) == manualKey) {
-            me.openPage(href.substring(manualKey.length));
+        if (href.substring(0, manualKey.length) == manualKey) {
+            var page = href.substring(manualKey.length);
+            me.openPage(page);
         } else {
             window.open(href);
         }
     });
+
+    console.log(this.back);
+
+    this.back.click(function back(e) {
+        if (this.history.length > 1) {
+            this.history.pop();
+            this.openPage(this.history.pop());
+        }
+    }.bind(this));
 }
 
 Manual.prototype = {
-    openPage: function(page) {
-        var me = this;
-
-        $.get('manual/' + this.lang + '/' + page + '.md', function(data) {
-            me.manual.html(marked(data));
-        });
+    openPage: function (page) {
+        $.get('manual/' + this.lang + '/' + page + '.md', function (data) {
+            this.history.push(page);
+            this.manual.html(marked(data));
+        }.bind(this));
     }
 }
