5608b7b4a1c806883f5bdfd29d9ce663606f4e56
Sami Kalliomäki <sami.kalliomaki@kapsi.fi>
2014-09-17 20:55:56 +0300

    New extend method
    New method is better supported by all browsers and can extend prototypes that already have content.

--- compiler/polyfill.js
+++ compiler/polyfill.js
@@ -91,15 +91,7 @@ if (!Array.prototype.indexOf) {
 
 function extend(a, b) {
     for (var i in b) {
-        var g = b.__lookupGetter__(i);
-        var s = b.__lookupSetter__(i);
-
-        if (g || s) {
-            if (g)
-                a.__defineGetter__(i, g);
-            if (s)
-                a.__defineSetter__(i, s);
-        } else {
+        if(!a.hasOwnProperty(i)) {
             a[i] = b[i];
         }
     }
