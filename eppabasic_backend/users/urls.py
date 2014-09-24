efa7907d96ac4e8ff50d20f0b72ad550f414151d
Sami Kalliomäki <sami.kalliomaki@kapsi.fi>
2014-09-25 01:25:15 +0300

    Fixes server crashing when confirming password reset

--- eppabasic_backend/users/urls.py
+++ eppabasic_backend/users/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     url(r'^register/', RegistrationView.as_view()),
     url(r'^get/', GetUserView.as_view()),
     url(r'^password_reset/', PasswordResetView.as_view()),
-    url(r'^confirm_reset/(?P<uidb64>[\d\w-_]+)/(?P<token>[\d\w-]+)/', password_reset_confirm, name='password_reset_confirm'),
+    url(r'^confirm_reset/(?P<uidb64>[\d\w\-_]+)/(?P<token>[\d\w-]+)/', password_reset_confirm, name='password_reset_confirm'),
     url(r'^complete_reset/', password_reset_complete, name='password_reset_complete'),
 )
