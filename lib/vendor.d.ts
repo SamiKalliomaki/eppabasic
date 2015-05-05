ff21b9537bd033fb3cf769565819ccbf2cc330ab
Sami Kalliomäki <sami.kalliomaki@kapsi.fi>
2015-05-05 17:53:00 +0300

    Fixes TypeScript compilation on v1.5.0-beta

--- lib/vendor.d.ts
+++ lib/vendor.d.ts
@@ -2,6 +2,7 @@
 // Hope that we can get rid of this file asap.
 // See: https://github.com/TypeStrong/grunt-ts/issues/216#issuecomment-83946550
 
+/// <reference path="lib" />
 /// <reference path="jquery" />
 /// <reference path="es6-promise" />
 /// <reference path="i18n" />
